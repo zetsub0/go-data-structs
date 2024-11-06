9fc54913ae73d6429c1d46393c3cd5b22137f2b5
zetsub0 <g.echilov@gmail.com>
2024-11-06 15:05:31 +0300

    go way naming. directories refactor

--- cmd/tests/main.go
+++ cmd/tests/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"github.com/zetsub0/go-data-structs/pkg/linkedList"
+	"github.com/zetsub0/go-data-structs/linkedList"
 )
 
 func main() {
-	ll := linkedList.New[int]()
-	ll.ParseArray([]int{1, 2, 3, 4, 5})
-	ll.Print()
+	ll := linkedlist.New[int]()
+
+	ll.Append(1)
 }
