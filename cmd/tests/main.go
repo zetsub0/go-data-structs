7e66d009c81d945517b506e1f370b7fc0f1c0305
zetsub0 <g.echilov@gmail.com>
2024-11-06 12:17:34 +0300

    changed linked list constructor. Added parseArray method.

--- cmd/tests/main.go
+++ cmd/tests/main.go
@@ -1,50 +1,11 @@
 package main
 
 import (
-	"log"
-
 	"github.com/zetsub0/go-data-structs/pkg/linkedList"
 )
 
 func main() {
-	ll := linkedList.New[any]([]any{any([]int{1, 2, 3, 4, 5}), any([]string{"asd", "bvf"})})
-	if err := ll.RemoveTail(); err != nil {
-		log.Println(err)
-	}
-	if err := ll.RemoveTail(); err != nil {
-		log.Println(err)
-	}
-	if err := ll.RemoveTail(); err != nil {
-		log.Println(err)
-	}
-	if err := ll.RemoveTail(); err != nil {
-		log.Println(err)
-	}
-	if err := ll.RemoveTail(); err != nil {
-		log.Println(err)
-	}
-	if err := ll.RemoveTail(); err != nil {
-		log.Println(err)
-	}
-	if err := ll.RemoveTail(); err != nil {
-		log.Println(err)
-	}
-	if err := ll.RemoveTail(); err != nil {
-		log.Println(err)
-	}
-	if err := ll.RemoveTail(); err != nil {
-		log.Println(err)
-	}
-
-	if err := ll.RemoveHead(); err != nil {
-		log.Println(err)
-	}
-
-	if err := ll.RemoveNode(12); err != nil {
-		log.Println(err)
-	}
-
-	if err := ll.RemoveNode(-1); err != nil {
-		log.Println(err)
-	}
+	ll := linkedList.New[int]()
+	ll.ParseArray([]int{1, 2, 3, 4, 5})
+	ll.Print()
 }
