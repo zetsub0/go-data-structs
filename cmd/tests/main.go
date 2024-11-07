e56ac9ee24138850281a351442384990bfcc1e29
zetsub0 <g.echilov@gmail.com>
2024-11-07 09:21:48 +0300

    Basic stack implementation

--- cmd/tests/main.go
+++ cmd/tests/main.go
@@ -1,11 +1,22 @@
 package main
 
-import (
-	"github.com/zetsub0/go-data-structs/pkg/linkedList"
-)
+import "fmt"
 
 func main() {
-	ll := linkedList.New[int]()
-	ll.ParseArray([]int{1, 2, 3, 4, 5})
-	ll.Print()
+	mainSlice := []int{1, 2, 3, 4, 5}
+	mainSlice = append(mainSlice, 6)
+	fmt.Println(len(mainSlice), cap(mainSlice))
+	r(mainSlice)
+	r(mainSlice)
+	r(mainSlice)
+	r(mainSlice)
+	r(mainSlice)
+
+	fmt.Println(len(mainSlice), cap(mainSlice))
+	fmt.Println(mainSlice[:cap(mainSlice)])
+}
+
+func r(s []int) {
+	s = append(s, 7)
+	fmt.Println(s)
 }
